1c115380a42491edaff93343c2c9cdb0755c584f
jacklq <877016605@qq.com>
2021-03-28 18:21:42 +0800

    原

---
+++ part/5-algorithm/src/main/java/com/lq/lqproj/LqprojApplication.java
@@ -0,0 +1,13 @@
+package com.lq.lqproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LqprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LqprojApplication.class, args);
+    }
+
+}
---
+++ part/5-algorithm/src/test/java/com/lq/lqproj/LqprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lq.lqproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LqprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
