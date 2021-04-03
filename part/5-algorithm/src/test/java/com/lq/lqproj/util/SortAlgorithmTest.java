0abfd10cc395740c2809886e2c3f7e4452b42b75
jacklq <877016605@qq.com>
2021-04-03 09:31:25 +0800

    增加算法测试

--- part/5-algorithm/src/test/java/com/lq/lqproj/util/SortAlgorithmTest.java
+++ part/5-algorithm/src/test/java/com/lq/lqproj/util/SortAlgorithmTest.java
@@ -22,25 +22,53 @@ class SortAlgorithmTest {
 
     @Test
     void sheelSort() {
+        System.out.println("希尔排序-sheelSort  start");
+        sortAlgorithm.sheelSort(array);
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("排序结束-sheelSort  end");
     }
 
     @Test
     void selectSort() {
+        System.out.println("选择排序-selectSort  start");
+        sortAlgorithm.selectSort(array);
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("排序结束-selectSort  end");
     }
 
     @Test
     void heapSort() {
+        System.out.println("堆排序-heapSort  start");
+        sortAlgorithm.selectSort(array);
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("堆结束-heapSort  end");
+
     }
 
     @Test
     void bubbleSort() {
+        System.out.println("堆排序-heapSort  start");
+        sortAlgorithm.bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println("堆结束-heapSort  end");
     }
 
     @Test
     void quickSort() {
+        System.out.println("快速排序-quickSort  start");
+        sortAlgorithm.quickSort(array,0,array.length-1);
+        System.out.println(Arrays.toString(array));
+        System.out.println("快速结束-quickSort  end");
     }
 
     @Test
     void mergeSort() {
+        System.out.println("合并排序-mergeSort  start");
+        sortAlgorithm.mergeSort(array,0,array.length-1);
+        System.out.println(Arrays.toString(array));
+        System.out.println("合并结束-mergeSort  end");
     }
 }
